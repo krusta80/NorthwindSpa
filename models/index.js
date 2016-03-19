417d6300662a6dde5dcd19180d6d237dbcb35f33
John Gruska <jag47@cornell.edu>
2016-03-18 20:07:55 -0400

    Spent the past 20 minutes adjusting the appointmnet schema to treat priorities as non-integer values

--- models/index.js
+++ models/index.js
@@ -3,7 +3,15 @@ mongoose.connect('mongodb://localhost');
 
 var appointmentSchema = new mongoose.Schema({
 	name: {type: String, required: true},
-	priority: {type: Number, required: true, min: 1}
+	priority: {type: Number, required: true, unique: true, min: 1}
+});
+
+appointmentSchema.virtual('priorityFloor').get(function() {
+	return Math.floor(this.priority);
+});
+
+appointmentSchema.set('toJSON', {
+	virtuals : true
 });
 
 appointmentSchema.pre('validate', function(next) {
@@ -12,6 +20,22 @@ appointmentSchema.pre('validate', function(next) {
 	next();
 });
 
+appointmentSchema.pre('save', function(next) {
+	if(this.priority === this.priorityFloor)
+		this.priority +=  getTS();
+	next();
+});
+
 var appointmentModel = mongoose.model('Appointment',appointmentSchema);
+
+function getTS() {
+	//	This returns ms since the epoch in decimal form,
+	//	which ensures that every appointment has a unique 
+	//	priority, even if the integer portion (priorityFloor)
+	//	repeats.  :)
+
+	var ts = new Date();
+	return ts/Math.pow(10,Math.ceil((Math.log(ts)/Math.log(10))));
+}
 
 module.exports = appointmentModel;
